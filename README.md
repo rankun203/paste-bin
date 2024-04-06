2fb0e0d73c9042718b4c4c164e9e8a43b68be82d
Kun Ran <rankun203@gmail.com>
2024-04-06 11:55:40 +1100

    feat: implement paste bin service with Docker support
    - create Dockerfile and docker-compose for service deployment
    - add .dockerignore and .gitignore to exclude node_modules
    - implement paste bin backend logic in index.js
    - add package.json with project dependencies
    - include pnpm-lock.yaml for package version locking
    - write README.md with basic usage instructions

---
+++ README.md
@@ -0,0 +1,24 @@
+# Paste Bin
+
+This tool is not meant for permanent content hosting, in fact it only keeps the data for 60s
+
+No requests are logged, ever.
+
+## Save Data
+
+```
+curl --request POST \
+  --url http://localhost:3000/set \
+  --header 'Content-Type: application/json' \
+  --data '{
+	"data": "long body"
+}'
+```
+
+Response will simply be:
+
+```
+http://localhost:3000/get/icLXssYVOkMDuSbwbRTrM
+```
+
+Query that URL and you will get the data in text format
---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const Redis = require("ioredis");
+const { nanoid } = require("nanoid");
+const morgan = require("morgan");
+
+const redis = new Redis(6379, "redis");
+// Check if Redis connection is successful
+redis.on("ready", () => {
+  console.log("Connected to Redis");
+});
+
+// Handle Redis connection errors
+redis.on("error", (error) => {
+  console.error("Error connecting to Redis:", error);
+  process.exit(1); // Exit the process with an error code
+});
+
+const setData = async (data) => {
+  const id = nanoid();
+  await redis.set(`bin:${id}`, data, "EX", 60);
+  return id;
+};
+
+const getData = async (id) => {
+  return await redis.get(`bin:${id}`);
+};
+
+const app = express();
+const port = 3000;
+
+app.set("trust proxy", 1); // Trust the first proxy
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  const protocol = req.get("X-Forwarded-Proto") || req.protocol;
+  const host = req.get("X-Forwarded-Host") || req.get("host");
+  const endpoint = `${protocol}://${host}`;
+  console.log("host:", endpoint); // Use or log the full URL as needed
+  app.locals.endpoint = endpoint;
+  next();
+});
+
+app.get("/get/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log(`go/${id}`);
+  const data = await getData(id);
+  res.header("Content-Type", "text/plain");
+  res.send(data);
+});
+
+app.post("/set", async (req, res) => {
+  const data = req.body.data;
+  const id = await setData(data);
+  res.send(`${app.locals.endpoint}/get/${id}`);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
